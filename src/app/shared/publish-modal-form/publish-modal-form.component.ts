3e066501898e1b586807e6fb859b8dcd5bedb7e1
wisnercelucus <wisnercelicus@gmail.com>
2020-07-01 15:29:02 -0400

    add username to post dialog

--- src/app/shared/publish-modal-form/publish-modal-form.component.ts
+++ src/app/shared/publish-modal-form/publish-modal-form.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Input, OnDestroy } from '@angular/core';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { UsersService } from 'src/app/users/users.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-publish-modal-form',
   templateUrl: './publish-modal-form.component.html',
   styleUrls: ['./publish-modal-form.component.css']
 })
-export class PublishModalFormComponent {
+export class PublishModalFormComponent implements OnDestroy{
+  @Input() username:string;
+  sub:Subscription;
+  
+
   constructor(
-    public dialogRef: MatDialogRef<PublishModalFormComponent>) {}
+    public dialogRef: MatDialogRef<PublishModalFormComponent>,
+    private usersService:UsersService) {
+      this.sub = this.usersService.loginUser.subscribe(
+        user=>{
+            this.username = user.username;
+        }
+      )
+    }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+  ngOnDestroy(){
+    if(this.sub){
+      this.sub.unsubscribe();
+    }
   }
 }
