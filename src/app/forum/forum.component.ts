7c69b2c6a67328d51c2a9c61548a62b1b3db9558
wisnercelucus <wisnercelicus@gmail.com>
2020-07-30 23:58:26 -0400

    fix a bug

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -28,8 +28,50 @@ export class UploadAdapter {
 })
 export class ForumComponent implements OnInit {
   public Editor = ClassicEditor;
+  config;
 
-  constructor() { }
+  constructor() {
+    this.config = {
+      toolbar: {
+        items: [
+          'heading',
+          '|',
+          'bold',
+          'italic',
+          'link',
+          'bulletedList',
+          'numberedList',
+          '|',
+          'indent',
+          'outdent',
+          '|',
+          'imageUpload',
+          'blockQuote',
+          'insertTable',
+          'mediaEmbed',
+          'undo',
+          'redo'
+        ]
+      },
+      image: {
+        toolbar: [
+          'imageStyle:full',
+          'imageStyle:side',
+          '|',
+          'imageTextAlternative'
+        ]
+      },
+      table: {
+        contentToolbar: [
+          'tableColumn',
+          'tableRow',
+          'mergeTableCells'
+        ]
+      },
+      // This value must be kept in sync with the language defined in webpack.config.js.
+      language: 'en'
+    };
+   }
 
   ngOnInit(): void {
   }
