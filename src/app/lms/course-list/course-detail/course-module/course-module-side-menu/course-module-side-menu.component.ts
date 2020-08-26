3d9d2c447ba188791d87731e21145cd65103987f
wisnercelucus <wisnercelicus@gmail.com>
2020-08-26 18:21:30 -0400

    add side nav to course module

--- src/app/lms/course-list/course-detail/course-module/course-module-side-menu/course-module-side-menu.component.ts
+++ src/app/lms/course-list/course-detail/course-module/course-module-side-menu/course-module-side-menu.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { faBirthdayCake, 
+  faMapMarker, 
+  faThumbsUp, 
+  faShareAltSquare, 
+  faComment, 
+  faHeart,
+ faSmile , 
+ faUserCircle, 
+ faUsers, 
+ faHome, 
+ faUser,
+ faComments
+} from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-course-module-side-menu',
@@ -6,10 +21,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-module-side-menu.component.css']
 })
 export class CourseModuleSideMenuComponent implements OnInit {
+  faBirthdayCake = faBirthdayCake;
+  faMapMarker = faMapMarker;
+  faThumbsUp = faThumbsUp;
+  faShareAltSquare = faShareAltSquare;
+  faComment = faComment;
+  faHeart = faHeart;
+  faSmile = faSmile;
+  faUserCircle = faUserCircle;
+  faUsers = faUsers;
+  faHome = faHome;
+  faUser = faUser;
+  faComments = faComments;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigateTo(path:string){
+    this.router.navigate([path])
   }
 
 }
